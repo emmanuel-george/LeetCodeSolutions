9326cefb3bab37a699ca3849e1b0f7b7370f9c41
Emmanuel George <65218643+emmanuel-george@users.noreply.github.com>
2022-05-25 16:13:57 -0600

    Find Minimum in Rotated Sorted Array BST 153 Medium

---
+++ venv/Find Minimum in Rotated Sorted Array BST 153.py
@@ -0,0 +1,21 @@
+def findMin(nums:List[int])->int:
+    N = len(nums)
+    start = 0
+    end = N -1
+    prev = 0
+    next = 0
+
+    while(start <= end):
+        if(nums[start] <= nums[end]):
+            return nums[start]
+        mid = start + (end - start)//2
+        next = (mid + 1) % N
+        prev = (mid + N - 1) % N
+
+        if(nums[mid] <= nums[prev] and nums[mid] <= nums[next]):
+            return nums[mid]
+        elif(nums[start] <= nums[mid]):
+            start = mid + 1
+        else:
+            end = mid - 1
+
