e0cbb8fc50a8e09d40e2cc8240669b7c493acb4e
Emmanuel George <65218643+emmanuel-george@users.noreply.github.com>
2022-05-25 19:49:25 -0600

    Find the number of time a sorted array is rotated BST

--- venv/Find the number of time the sorted array is rotated.py
+++ venv/Find the number of time the sorted array is rotated.py
@@ -1,23 +1,33 @@
-def findNoTimesRotated(nums: List[int]):
-     N = len(nums)
-     start = 0
-     end = N-1
-     prev =0
-     next =0
 
-     while(start <= end):
-         if(nums[start] <= nums[end]):
-             return nums[start]
 
-         mid = start + (end - start)//2
-         next = (mid +1) % N
-         prev = (mid + N -1) % N
-         if(nums[mid] <= nums[prev] and nums[mid] <= nums[next]):
-             return mid
-         elif(nums[start] <= nums[mid]):
-             start = mid + 1
-         else:
-             end = mid -1
+def findMinIndex( nums, target) -> int:
+    print(nums)
+    N = len(nums)
+    start = 0
+    end = N - 1
+    prev = 0
+    nex = 0
+
+    while (start <= end):
+        mid = start + (end - start) // 2
+        nex = (mid + 1) % N
+        prev = (mid + N - 1) % N
+
+        if (nums[mid] <= nums[prev] and nums[mid] <= nums[nex]):
+            return mid
+        elif (nums[start] <= nums[mid] and nums[mid] >= nums[N-1]):
+            start = mid+1 % N
+        else:
+            end = (mid + N - 1) % N
+    return -1
+
+if __name__ == '__main__':
+    nums = [4,5,6,7,0,1,2]
+    findMinIndex()
+
+
+
+
 
 
 
